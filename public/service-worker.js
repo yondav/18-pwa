127480eca438b1ddaad9f3739508919964a67d08
yondav <yonidavid.abr@gmail.com>
2021-06-07 13:11:32 -0400

    service worker and manifest

---
+++ public/service-worker.js
@@ -0,0 +1,67 @@
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/styles.css',
+  '/index.js',
+  '/db.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+];
+
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
+
+// install
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Success - Files pre-cached');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log('Removing old cache data', key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener('fetch', (evt) => {
+  if (evt.request.url.includes('/api/transaction ')) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then(async (cache) => {
+          try {
+            const response = await fetch(evt.request);
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+            return response;
+          } catch (err) {
+            return await cache.match(evt.request);
+          }
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
+  evt.respondWith(caches.match(evt.request).then((response) => response || fetch(evt.request)));
+});
